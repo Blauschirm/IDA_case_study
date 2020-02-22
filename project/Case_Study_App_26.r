07c3662d5c67d2a48ede9ddaedd92180980fd57d
Jan Kursawe <jankursawe@yahoo.de>
2020-02-22 19:44:41 +0100

    data table mit selectize input menüs ausgetauscht, Filter zurücksetzen fehlt noch

--- project/Case_Study_App_26.r
+++ project/Case_Study_App_26.r
@@ -29,26 +29,7 @@ load("Datensatz_tidy.RData")
 # Data preperation
 #
 final_joined <- head(final_joined, n = 500)
-# Filter rows to display only distinct ID_Fahrzeug values: fahrzeuge
-fahrzeuge <- final_joined[!duplicated(final_joined$ID_Fahrzeug),]
-#
-# Create a search/autocomplete vector with ID_Einzelteil, ID_Komponente and ID_Fahrzeug: inputID
-#inputIDs <- c(fahrzeuge$ID_Fahrzeug, final_joined$ID_Einzelteil, final_joined$ID_Sitze)
-#inputIDs <- fahrzeuge$ID_Fahrzeug[1:10]
-fahrzeuge_subset <- fahrzeuge[1:40,]
-#
-# Cerate a search/autocomplete list as group options: inputIDs_grouped
-#inputIDs_grouped <- list(ID_Einzelteil = final_joined$ID_Einzelteil, ID_Komponente = final_joined$ID_Sitze, ID_Fahrzeug = fahrzeuge$ID_Fahrzeug)
-#
-# Cerate a subset of IDs (due to performance issues)
-subset_size = 120000
-subset_distribution_size <- subset_size / 3
-inputIDs_grouped <- list(ID_Einzelteil = final_joined$ID_Einzelteil[1:subset_distribution_size], ID_Komponente = final_joined$ID_Sitze[1:subset_distribution_size], ID_Fahrzeug = fahrzeuge$ID_Fahrzeug[1:subset_distribution_size])
-#str(inputIDs_grouped)
-# Cerate a subset of 30k IDs (due to performance issues)
-#inputIDs_grouped <- list(ID_Einzelteil = final_joined$ID_Einzelteil[1:10000], ID_Komponente = final_joined$ID_Sitze[1:10000], ID_Fahrzeug = fahrzeuge$ID_Fahrzeug[1:10000])
 
-#str(inputIDs_grouped) # Stats
 # Shiny UI
 ui <- fluidPage(
   mainPanel(
@@ -58,22 +39,20 @@ ui <- fluidPage(
       plotOutput("plot_zulassungsverlauf")
     ),
     wellPanel(
-      titlePanel("Darstellung 1: Karte"),
+      titlePanel("Darstellung 2: Heatmap mit Gemeinde-Suche und Bauteil-Suche"),
       fluidRow(
         column(12,
           fluidRow(
             column(12,
-              titlePanel("Darstellung 2: Heatmap mit Fahrzeug-Suche und Bauteil-Suche + Darstellung des Lieferwegs"),
               fluidRow(
                 actionButton("reset_filters", "Alle Filter zurücksetzen")
               ),
               fluidRow(
-                column(
-                  3, 
-                  "betroffene Gemeinden",
+                column(3, 
+                  (h4("Betroffene Gemeinden")),
                   
                   # Display Betroffene Gemeinden as data table
-                  DT::dataTableOutput('datatable_gemeinden'),
+                  dataTableOutput('datatable_gemeinden'),
                   
                   #datatable(datatable_gemeinde), # slow
                   # depreciated due to client-side rendering!
@@ -82,71 +61,43 @@ ui <- fluidPage(
                   # server-side: output$'my_table' <- renderDataTable(my_df)
                   
                 ),
-                column(
-                  7,
+                column(7,
                   fluidRow(
                     
                     # Heatmap with search bar section
-                    column(8, 
-                      titlePanel(h6("Zum Anzeigen von Fahrzeuginformationen hineinzoomen und/oder auf die Markierungen klicken")),
+                    column(10,
+                      (h4("Betroffene Bauteile")),
                       
-                      # Create search input bar with autocomplete: selectizeInput()
-                      # from: https://shiny.rstudio.com/gallery/selectize-examples.html
-                      # if needed: toggle/hide Dropdown: https://rdrr.io/cran/shinyWidgets/man/toggleDropdownButton.html
-                      # if neeeed: performance boost: check selectizeInput('x2'... https://shiny.rstudio.com/gallery/option-groups-for-selectize-input.html
+                      # Display ID-search by ID_einzelteile & ID_sitze
+                      dataTableOutput('datatable_bauteile'),
                       
-                      # selectizeInput() (depreciated)
+                      # Select map type
                       selectizeInput(
                         'e1', 'Wählen Sie den Kartentyp aus',
                         choices = c("Fahrzeuginfo", "Lieferweg des Bauteils")
                       ),
-                      # selectizeInput(
-                      #   'e2', 'Wählen Sie eine oder mehrere Fahrzeug- oder Bauteil-IDs aus',
-                      #   choices = inputIDs_grouped, multiple = TRUE, options = list(maxOptions = 6, placeholder = 'Filter für ID_Bauteil(e) und/oder ID_Fahrzeug (AND Verknüpfung)'
-                      # ),
-                      
-                      # Client-side rendering of updateSelectizeInput(session, 'search_by_ID2', ...),
-                      selectizeInput('search_by_ID', 'Wählen Sie eine oder mehrere Fahrzeug- oder Bauteil-IDs aus',
-                                     choices = NULL,
-                                     multiple = TRUE,
-                      ),
 
-                      
-                      # Highlight the text and use CTRL + SHIFT + C to (un)comment multiple lines in Windows. Or, command + SHIFT + C in OS-X.
-                      # selectizeInput(
-                      #   'e3', '3. Item creation', choices = inputIDs,
-                      #   options = list(create = TRUE)
-                      # ),
-                      # selectizeInput(
-                      #   'e4', '4. Max number of options to show (3)', choices = inputIDs,
-                      #   options = list(maxOptions = 3)
-                      # ),
-                      # selectizeInput(
-                      #   'e5', '5. Max number of items to select', choices = inputIDs,
-                      #   multiple = TRUE, options = list(maxItems = 2)
-                      # ),
-                      
                       # Display the heatmap  with car markers
-                      leafletOutput(outputId = "map", width = 600, height = 600)
+                      leafletOutput(outputId = "map", width = '100%', height = 600)
                       ),
-                      column(8, "Bottombox")
+                      column(10, "Zum Anzeigen von Fahrzeuginformationen hineinzoomen und/oder auf die Markierungen klicken")
                   )
                 ),
-                column(2,
-                  fluidRow("Betroffene Fahrzeuge"),
-                  
-                    # Adds 'Betrofene Fahrzeuge' table 
-                    column(12,
-                         tableOutput('table_fahrzeuge'),
-                  ),
-                  
-                  fluidRow("Betroffene Bauteile"),
-                  
-                    # Adds 'Betrofene Bauteile' table
-                    column(12,
-                         tableOutput('table_bauteile'),
-                  ),
-                )
+                # column(2,
+                #   fluidRow("Betroffene Fahrzeuge"),
+                #   
+                #     # Adds 'Betrofene Fahrzeuge' table 
+                #     column(12,
+                #          tableOutput('table_fahrzeuge'),
+                #   ),
+                #   
+                #   fluidRow("Betroffene Bauteile"),
+                #   
+                #     # Adds 'Betrofene Bauteile' table
+                #     column(12,
+                #          tableOutput('table_bauteile'),
+                #   ),
+                # )
               )
             )
           )
@@ -161,6 +112,9 @@ ui <- fluidPage(
 
 # Shiny Server
 server <- function(input, output, session) {
+
+  # Filter rows to display only distinct ID_Fahrzeug values: fahrzeuge
+  fahrzeuge <- final_joined[!duplicated(final_joined$ID_Fahrzeug),]
   
   zulassungen <- reactive({
     if(length(input$datatable_gemeinden_rows_selected)){
@@ -186,20 +140,8 @@ server <- function(input, output, session) {
                    limits = as.Date(xlim))
   })
   
-  # Render dropdown menu: selectizeInput()
-  # with inputID: 'search_by_ID'
-  # https://shiny.rstudio.com/gallery/option-groups-for-selectize-input.html
-  # https://shiny.rstudio.com/reference/shiny/0.14/updateSelectInput.html
-  updateSelectizeInput(session, 'search_by_ID', 
-                            choices = inputIDs_grouped,
-                            #multiple = TRUE, 
-                            options = list(
-                              maxOptions = 6,
-                              placeholder = 'Filter für ID_Bauteil(e) und/oder ID_Fahrzeug (AND Verknüpfung)',
-                              selected = NULL)
-  )
   
-  # Render table: datatable_gemeinde, table_fahrzeuge, table_bauteile
+  # Render data tables: gemeinden / bauteile
   # https://shiny.rstudio.com/reference/shiny/latest/renderTable.html
   # https://shiny.rstudio.com/reference/shiny/0.12.1/tableOutput.html
   
@@ -210,12 +152,18 @@ server <- function(input, output, session) {
     arrange(Gemeinde) %>%
     ungroup()
   
+  # Render data table: gemeinden
   output$datatable_gemeinden <- renderDataTable({
                                                 input$reset_filters
                                                 gemeinden}) # Betroffene Gemeinde
-  output$table_fahrzeuge <- renderTable(fahrzeuge_subset[1:10, "ID_Fahrzeug"]) # Betroffene Fahrzeuge
-  output$table_bauteile <- renderTable(final_joined[1:30, "ID_Einzelteil"]) # Betroffene Bauteile
-  
+  # Render data table: bauteile
+  # fehlt noch: input$reset_filters
+  output$datatable_bauteile <- renderDataTable(final_joined[final_joined$Fehlerhaft_Einzelteil == 1 | final_joined$Fehlerhaft_Komponente == 1, c(1, 2, 4, 7, 11, 13)], # [1:30,c(1,4)],  # Betroffene Bauteile
+                                               options = list(
+                                                 lengthMenu = list(c(3, 6, -1), c('3', '6', 'All')),
+                                                 pageLength = 3
+                                               ),
+                                               rownames = FALSE) # suppress row names / index numbers
   
   filtered_vehicles <- reactive({
     if(length(input$datatable_gemeinden_rows_selected)){
